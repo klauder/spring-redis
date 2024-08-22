2214833222b1c29cb2b16c52bf91ade80b679db3
Klauder <klauderdias@gmail.com>
2024-08-22 14:54:00 -0300

    first commit

---
+++ src/test/java/com/klauderdias/spring/redis/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klauderdias.spring.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
